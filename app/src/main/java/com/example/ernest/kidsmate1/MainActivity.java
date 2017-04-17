c61178b02f3717db9ff2d46721b2ff82aac2b6b0
collarhub <june4145@naver.com>
2017-04-17 13:54:47 +0900

    switch activity test

--- app/src/main/java/com/example/ernest/kidsmate1/MainActivity.java
+++ app/src/main/java/com/example/ernest/kidsmate1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ernest.kidsmate1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    protected void mainStartClicked(View v) {
+        Intent intent = new Intent(MainActivity.this, SelectContents.class);
+        startActivity(intent);
+        finish();
+    }
+
+    protected void mainOffClicked(View v) {
+        finish();
+        System.exit(0);
     }
 }
